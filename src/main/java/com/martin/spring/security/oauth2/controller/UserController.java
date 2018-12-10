9f8349eaf3f063fbe4cd643fbd922cd5c4b3f401
Martin Luccanera <martin.luccanera@gmail.com>
2018-12-10 00:22:39 -0300

    Adding post new data capabilities

--- src/main/java/com/martin/spring/security/oauth2/controller/UserController.java
+++ src/main/java/com/martin/spring/security/oauth2/controller/UserController.java
@@ -14,13 +14,23 @@ import java.util.LinkedHashMap;
 @RestController
 @RequestMapping("/secured/user")
 public class UserController {
-    private static final String FETCH_PROFILE_FIELDS = "SELECT * FROM user_profile WHERE username = ?";
-    //private static final String[] columns = {"name", "username", "email", "bio", "birthdate");
+    private static final String FETCH_PROFILE_FIELDS =
+            "SELECT * " +
+            "FROM user_profile " +
+            "WHERE id IN ( " +
+                "SELECT MAX(id) " +
+                "FROM user_profile " +
+                "WHERE username = ? " +
+            ") ";
+    private static final String UPDATE_PROFILE_FIELDS =
+            "INSERT INTO user_profile(username, name, email, bio, birthdate, date_updated) " +
+            "VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
 
     @RequestMapping(value = "/profile", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody
-    String getUserProfile(@RequestParam String username) { Connection con = null;
+    String getUserProfile(@RequestParam String username) {
+        Connection con = null;
         PreparedStatement statement = null;
         LinkedHashMap<String, String> result = new LinkedHashMap<>();
         try{
@@ -63,5 +73,46 @@ public class UserController {
             }//end finally try
         }//end try
         return convertToJson(result);
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public @ResponseBody
+    void updateUserProfile(@RequestParam String username, String name, String email, String bio, String birthdate) {
+        Connection con = null;
+        PreparedStatement statement = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            //STEP 3: Open a connection
+            con = DriverManager.getConnection(DB_URL, USER, PASS);
+            //STEP 4: Execute a query
+            statement = con.prepareStatement(UPDATE_PROFILE_FIELDS);
+            statement.setString(1, username);
+            statement.setString(2, name);
+            statement.setString(3, email);
+            statement.setString(4, bio);
+            statement.setDate(5, Date.valueOf(birthdate));
+            statement.executeUpdate();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(statement!=null)
+                    con.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(con!=null)
+                    con.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
     }
 }
